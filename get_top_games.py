7be411ebef330a24369f49c60113c17b78bdcbcd
egapay <etg5588@rit.edu>
2024-11-12 10:51:20 -0500

    pulls in data to csv and SQL Server db now

--- get_top_games.py
+++ get_top_games.py
@@ -1,8 +1,18 @@
 import requests
 import locale
 import pandas as pd
+import sqlalchemy
 
 apiEndpoint = "https://steamspy.com/api.php?request=top100forever"
+
+server = 'localhost'
+database = 'game'
+port = 1433
+username = 'user'
+password = 'user'
+table = 'game_data'
+
+engine = sqlalchemy.create_engine(f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server')
 
 response = requests.get(apiEndpoint)
 data = response.json()
@@ -19,17 +29,16 @@ for appid, appInfo in data.items():
         "negative":appInfo.get("negative"),
         "price":locale.currency(int(appInfo.get("price"))/100),
         "owners":appInfo.get("owners"),
-        "averagePlaytime":appInfo.get("average_forever"),
-        "medianPlaytime":appInfo.get("median_forever")
+        "average_playtime":appInfo.get("average_forever"),
+        "median_playtime":appInfo.get("median_forever")
     })
 
 df = pd.DataFrame(dataList)
-# print(df)
 
-df.to_csv('topgames.csv', index=False)
+try:
+    df.to_csv('topgames.csv', index=False)
+    # Connect to the SQL Server database
+    df.to_sql('game_data', con = engine, if_exists='replace', chunksize=1000)
 
-# df = pd.DataFrame(data)
-# df = df[df['name'].notna() & (df['name'] != '')]
-
-# df.to_csv('gamelist.csv', index=False)
-
+except Exception as e:
+    print(f"An error occurred: {e}")
