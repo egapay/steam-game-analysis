02093718a81f2e066286c6cc02b4f55333677fa4
ethangapay <ethantgapay@gmail.com>
2024-12-10 13:06:31 -0500

    removed unsused files

--- src/py/get_top_games.py
+++ src/py/get_top_games.py
@@ -1,74 +1,46 @@
 import requests
 import locale
 import pandas as pd
-import psycopg2 as pgDB
-import os
-from dotenv import load_dotenv
-from sqlalchemy import create_engine
+import sqlalchemy
 
 apiEndpoint = "https://steamspy.com/api.php?request=top100forever"
 
-load_dotenv()
+server = '<server>'
+database = '<db>'
+port = 1433
+username = '<username>'
+password = '<password>'
+table = '<table>'
 
-db_name = os.getenv('DB_NAME')
-db_table = os.getenv('DB_TABLE')
-db_user = os.getenv('DB_USER')
-db_password = os.getenv('DB_PASSWORD')
-db_host = os.getenv('DB_HOST')
+engine = sqlalchemy.create_engine(f'mssql+pyodbc://{username}:{password}@{server}/{database}?driver=ODBC+Driver+17+for+SQL+Server')
 
-conn_string = f'postgresql://{db_user}:{db_password}@{db_host}/{db_name}'
-db = create_engine(conn_string)
+response = requests.get(apiEndpoint)
+data = response.json()
 
-conn = db.connect()
+dataList = []
+locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
-# try:
-#     conn = pgDB.connect(
-#         database = db_name,
-#         user = db_user,
-#         password = db_password,
-#         host = db_host,
-#         port = 5432,
-#     )
-# except Exception as e:
-#     print(e)
-#     exit(0)
+for appid, appInfo in data.items():
+    dataList.append({
+        "appid":appid,
+        "name":appInfo.get("name"),
+        "positive":appInfo.get("positive"),
+        "positive_percentage": round((appInfo.get("positive") / (appInfo.get("positive") + appInfo.get("negative")) * 100), 2),
+        "negative":appInfo.get("negative"),
+        "negative_percentage": round((appInfo.get("negative") / (appInfo.get("positive") + appInfo.get("negative")) * 100), 2),
+        "total_reviews": appInfo.get("positive") + appInfo.get("negative"),
+        "price":int(appInfo.get("price"))/100,
+        "owners":appInfo.get("owners"),
+        "average_playtime_hours": round((appInfo.get("average_forever") / 60), 0),
+        "median_playtime_hours": round((appInfo.get("median_forever") / 60), 0),
+    })
 
-# cursor = conn.cursor()
-# cursor.execute("SELECT version()")
+df = pd.DataFrame(dataList)
 
-# data = cursor.fetchone()
-# print("DATA: ", data)
+try:
+    df.to_csv('topgames.csv', index=False)
+    # Connect to the SQL Server database
+    df.to_sql('game_data', con = engine, if_exists='replace', chunksize=1000)
 
-conn.close()
-
-
-# response = requests.get(apiEndpoint)
-# data = response.json()
-
-# dataList = []
-# locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
-
-# for appid, appInfo in data.items():
-#     dataList.append({
-#         "appid":appid,
-#         "name":appInfo.get("name"),
-#         "positive":appInfo.get("positive"),
-#         "positive_percentage": round((appInfo.get("positive") / (appInfo.get("positive") + appInfo.get("negative")) * 100), 2),
-#         "negative":appInfo.get("negative"),
-#         "negative_percentage": round((appInfo.get("negative") / (appInfo.get("positive") + appInfo.get("negative")) * 100), 2),
-#         "total_reviews": appInfo.get("positive") + appInfo.get("negative"),
-#         "price":int(appInfo.get("price"))/100,
-#         "owners":appInfo.get("owners"),
-#         "average_playtime_hours": round((appInfo.get("average_forever") / 60), 0),
-#         "median_playtime_hours": round((appInfo.get("median_forever") / 60), 0),
-#     })
-
-# df = pd.DataFrame(dataList)
-
-# try:
-#     df.to_csv('topgames.csv', index=False)
-#     # Connect to the SQL Server database
-#     df.to_sql('game_data', con = engine, if_exists='replace', chunksize=1000)
-
-# except Exception as e:
-#     print(f"An error occurred: {e}")
+except Exception as e:
+    print(f"An error occurred: {e}")
